0d9ee62c43953a2c6f08a289f6e9f4647b9b9a40
Ruan van der Merwe <ruanvdmerwe@outlook.com>
2020-01-20 19:55:22 +0200

    fixed errors

--- MiipetsWebApp/listings/urls.py
+++ MiipetsWebApp/listings/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('all-listing/', listing_views.view_all_listings, name='listings-all'),
+    path('listings/<str:type>', listing_views.view_listings, name='listings')
+    #path('single-listing/<int:service_id>', listing_views.view_single_listings, name='listings-single'),
 ]
 
 if settings.DEBUG:
