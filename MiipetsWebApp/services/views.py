d2701c2510f5bd10933efd12252b9f90b2cd085e
Ruan van der Merwe <ruanvdmerwe@outlook.com>
2020-01-31 08:32:50 +0200

    user can now filter on price as well when in services

--- MiipetsWebApp/services/views.py
+++ MiipetsWebApp/services/views.py
@@ -60,14 +60,38 @@ def view_services(request, type):
         except:
             start_date, end_date = sort_out_dates('', '')
 
+        try:
+            price_start = request.GET['price_start']
+            price_end = request.GET['price_end']
+
+            if price_start > price_end:
+                price_start, price_end = price_end, price_start
+
+            if not price_start:
+                price_start = 0
+                price_end = 99999999
+        except:
+            price_start = 0
+            price_end = 99999999
+
         #get relevant services not based on location
         services = SitterServices.objects.filter(Q(type__in=type)&
                                                  Q(date_start__lte=start_date)&
-                                                 Q(date_end__gte=end_date))
+                                                 Q(date_end__gte=end_date)&
+                                                 Q(price__range=[price_start, price_end]))
 
 
         #filter on location
-        services,locations = filter_on_location(services, request.GET['location_input'])
+        try:
+            location_input = request.GET['location_input']
+            services,locations = filter_on_location(services, location_input)
+        except:
+            location_input = ""
+            ids = [service.id for service in services]
+            locations = ServiceLocation.objects.filter(id__in=ids)
+
+        if not location_input:
+            location_input = "Location"
 
         services = zip(services, locations)
 
@@ -77,23 +101,27 @@ def view_services(request, type):
                     "title": "View pet services",
                     "type":"Services",
                     "sitter_user":True,
-                    "services":services
+                    "services":services,
+                    "location_input":location_input
                     }
             else:
                 context = {
                     "title": "View pet services",
                     "type":"Services",
                     "sitter_user":False,
-                    "services":services
+                    "services":services,
+                    "location_input":location_input
                     }
         except:
             context = {
                 "title": "View pet services",
                 "type":"Services",
                 "sitter_user":False,
-                "services":services
+                "services":services,
+                "location_input":location_input
                 }
 
+    print(context)
     return render(request, 'services/single-type-services.html', context)
 
 
