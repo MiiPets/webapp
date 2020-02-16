8c2185e7262654937c2a552a390a2b1db8153a8d
Ruan van der Merwe <ruanvdmerwe@outlook.com>
2020-02-16 19:57:26 +0200

    services are now seen and filtered on review score

--- MiipetsWebApp/services/views.py
+++ MiipetsWebApp/services/views.py
@@ -125,24 +125,40 @@ def view_services(request, type):
             want_reptile = False
 
 
+
+        try:
+            review_score = request.GET['review_score']
+            if review_score == "Review score":
+                review_score = -1
+            else:
+                print(review_score[0])
+                print(review_score[0])
+                review_score = int(review_score[0])
+
+        except:
+            review_score = -1
+
+
         #get relevant services not based on location
         if pet_type == "All Pets":
             services = SitterServices.objects.filter(Q(type__in=type)&
                                                      Q(allowed_to_show=True)&
-                                                     Q(date_start__lte=start_date)&
-                                                     Q(date_end__gte=end_date)&
-                                                     Q(price__range=[price_start, price_end]))
+                                                     Q(date_start__gte=start_date)&
+                                                     Q(date_end__lte=end_date)&
+                                                     Q(price__range=[price_start, price_end])&
+                                                     Q(review_score__gte=review_score))
         else:
             services = SitterServices.objects.filter(Q(type__in=type)&
                                                      Q(allowed_to_show=True)&
-                                                     Q(date_start__lte=start_date)&
-                                                     Q(date_end__gte=end_date)&
+                                                     Q(date_start__gte=start_date)&
+                                                     Q(date_end__lte=end_date)&
                                                      Q(price__range=[price_start, price_end])&
                                                      Q(dogs_allowed=want_dog)&
                                                      Q(cats_allowed=want_cat)&
                                                      Q(birds_allowed=want_bird)&
                                                      Q(reptiles_allowed=want_reptile)&
-                                                     Q(other_pets_allowed=want_other))
+                                                     Q(other_pets_allowed=want_other)&
+                                                     Q(review_score__gte=review_score))
 
         #filter on location
         try:
